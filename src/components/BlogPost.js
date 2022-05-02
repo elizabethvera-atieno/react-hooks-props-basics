ca14b0325f9e9180bf3b7567f16acdbd9a361925
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-02 18:11:45 +0300

    code allong

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,14 +3,22 @@ import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
 function BlogPost() {
-  return (
+  // return (
+  //   <div id="blog-post">
+  //     <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+  //     <Comment commentText="I agree with this statement. - Angela Merkel" />
+  //     <Comment commentText="A universal truth. - Noam Chomsky" />
+  //     <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+  //   </div>
+  // );
+  return(
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText = "Dear Reader: Bjarne Stroustrup has the perfect lecture oration."/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
-  );
+  )
 }
 
 export default BlogPost;
